90d3c0ac42f79a0106dda80ec6b40882cd18ceba
Simuzax <simuzax@gmail.com>
2019-07-31 19:22:43 -0300

    Menu - 190731

--- ExercicioEndless/Assets/Scripts/Game.cs
+++ ExercicioEndless/Assets/Scripts/Game.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
+    public GameObject fundo;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(fundo);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void loadGame()
+    {
+        SceneManager.LoadScene("Jogo");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
     }
 }

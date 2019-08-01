8fc011b219008d36015b7059069f226669d852ba
Simuzax <simuzax@gmail.com>
2019-08-01 12:32:23 -0300

    Pooling

--- ExercicioEndless/Assets/Scripts/Game.cs
+++ ExercicioEndless/Assets/Scripts/Game.cs
@@ -6,6 +6,23 @@ using UnityEngine.SceneManagement;
 public class Game : MonoBehaviour
 {
     public GameObject fundo;
+    public GameObject tile;
+
+    /* Array spawns[5]
+    lane = random(0, 4);
+    position = spawns[lane].position;
+    */
+
+    [SerializeField]
+    int maxQueue;
+
+    Queue<Tile> tiles = new Queue<Tile>();
+
+    [SerializeField]
+    Vector2 distanceBetweenTiles;
+
+    [SerializeField]
+    Vector2 firstPosition;
 
     private void Awake()
     {
@@ -16,7 +33,24 @@ public class Game : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        distanceBetweenTiles.x += tile.GetComponent<SpriteRenderer>().bounds.size.x;
 
+        for (int i = 0; i < maxQueue; i++)
+        {
+            Tile t = Instantiate(tile, Vector2.zero, Quaternion.identity).GetComponent<Tile>();
+
+            Vector2 position = firstPosition;
+
+            t.lane = Random.Range(0, 4);
+
+            position.x += t.lane * distanceBetweenTiles.x;
+
+            position.y += i * distanceBetweenTiles.y;
+
+            t.transform.position = position;
+
+            tiles.Enqueue(t);
+        }
     }
 
     // Update is called once per frame
@@ -33,5 +67,10 @@ public class Game : MonoBehaviour
     public void exitGame()
     {
         Application.Quit();
+    }
+
+    public void respawn()
+    {
+        //Dequeue, Enqueue
     }
 }
---
+++ ExercicioEndless/Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public int lane;
+    float speed = 5f;
+    Game gameRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameRef = GameObject.FindGameObjectWithTag("Game").GetComponent<Game>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, (float)-0.05, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Parede"))
+        {
+            gameRef.respawn();
+        }
+    }
+}

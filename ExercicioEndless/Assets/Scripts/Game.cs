35ed2903d10f475e700fc9635f0ef714c7abe87a
Simuzax <simuzax@gmail.com>
2019-08-02 12:39:35 -0300

    190802

--- ExercicioEndless/Assets/Scripts/Game.cs
+++ ExercicioEndless/Assets/Scripts/Game.cs
@@ -5,72 +5,112 @@ using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour
 {
+    //Timer
+    [SerializeField]
+    float timerMax;
+    float timer;
+
+    //Não Remover
     public GameObject fundo;
     public GameObject tile;
 
-    /* Array spawns[5]
-    lane = random(0, 4);
-    position = spawns[lane].position;
-    */
+    //Spawns
+    Transform[] spawns = new Transform[4];
 
-    [SerializeField]
-    int maxQueue;
-
+    //Tiles
     Queue<Tile> tiles = new Queue<Tile>();
 
+    //Lanes
     [SerializeField]
-    Vector2 distanceBetweenTiles;
+    int maxQueue;
+    int maxLane = 4;
 
-    [SerializeField]
-    Vector2 firstPosition;
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Jogo")
+        {
+            GameObject[] spawnsGO = GameObject.FindGameObjectsWithTag("spawnPoints");
+
+            for(int i = 0; i < spawnsGO.Length;i++)
+            {
+                if (i >= spawns.Length)
+                    break;
+
+                spawns[i] = spawnsGO[i].transform;
+            }
+
+            spawn();
+        }
+    }
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
         DontDestroyOnLoad(fundo);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void spawn()
+    {
+        if (tiles.Count >= maxQueue)
+        {
+            return;
+        }
+
+        int lane = Random.Range(0, maxLane);
+
+        Vector3 position = spawns[lane].position;
+
+        Tile t = Instantiate(tile, position, Quaternion.identity).GetComponent<Tile>();
+
+        t.lane = lane;
+
+        tiles.Enqueue(t);
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        distanceBetweenTiles.x += tile.GetComponent<SpriteRenderer>().bounds.size.x;
-
-        for (int i = 0; i < maxQueue; i++)
-        {
-            Tile t = Instantiate(tile, Vector2.zero, Quaternion.identity).GetComponent<Tile>();
-
-            Vector2 position = firstPosition;
-
-            t.lane = Random.Range(0, 4);
-
-            position.x += t.lane * distanceBetweenTiles.x;
-
-            position.y += i * distanceBetweenTiles.y;
-
-            t.transform.position = position;
-
-            tiles.Enqueue(t);
-        }
+        timer = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time >= timer + timerMax && tiles.Count < maxQueue)
+        {
+            timer += Time.time;
 
+            spawn();
+        }
     }
 
+    //Menu Start
     public void loadGame()
     {
         SceneManager.LoadScene("Jogo");
     }
 
+    //Menu Exit
     public void exitGame()
     {
         Application.Quit();
     }
 
+    //Pooling dos Tiles
     public void respawn()
     {
-        //Dequeue, Enqueue
+        int lane = Random.Range(0, maxLane);
+        Tile t = tiles.Dequeue();
+
+        while (lane == t.lane)
+        {
+            lane = Random.Range(0, maxLane);
+        }
+
+        t.lane = lane;
+        t.transform.position = spawns[t.lane].position;
+        tiles.Enqueue(t);
     }
 }
